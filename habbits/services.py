9576f68075d24aaa517d49b86b8dd16c9e7332bb
Vlad Morozov <morozov90vlad@mail.ru>
2024-05-23 19:27:39 +0300

    Small fixes 2

--- habbits/services.py
+++ habbits/services.py
@@ -3,11 +3,14 @@ from datetime import datetime, timedelta
 
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
+from users.models import User
+
 
 def set_schedule_for_reminder(obj):
     """Создает периодическую задачу, принимает экземплял класса."""
     time = datetime.strptime(obj["execution_time"], "%H:%M:%S") - timedelta(minutes=15)
-    message = f"Через 15 минут вам пора [{obj["action"]}]"
+    telegram_chat_id = User.objects.get(id=obj["owner"]).tg_id
+    message = f"Через 15 минут вам пора [{obj["action"]}],{telegram_chat_id}"
 
     schedule, _ = CrontabSchedule.objects.get_or_create(
         minute=int(time.minute),
--- habbits/tasks.py
+++ habbits/tasks.py
@@ -1,47 +1,53 @@
 import requests
 from celery import shared_task
 
-from config.settings import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN, TELEGRAM_URL
+from config.settings import TELEGRAM_TOKEN, TELEGRAM_URL
 from habbits.models import Habit
+from users.models import User
 
 
 @shared_task
 def send_telegram_start_message_award(obj):
     """Отправляет сообщение, если выбрано вознаграждение."""
+    telegram_chat_id = User.objects.get(id=obj["owner"]).tg_id
     message = (
         f"Вы запланировали привычку [{obj["action"]}]! "
         f"в [{obj["execution_time"]}] в [{obj["place"]}], "
         f"[{obj["periodicity"]}] раз в неделю! "
         f"Вознаграждение [{obj["award"]}]"
     )
-    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}")
+    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={telegram_chat_id}&text={message}")
 
 
 @shared_task
 def send_telegram_start_message_connected_habit(obj):
     """Отправляет сообщение, если выбрана связанная привычка."""
     con_habit = Habit.objects.get(id=obj["connected_habit"])
+    telegram_chat_id = User.objects.get(id=obj["owner"]).tg_id
     message = (
         f"Вы запланировали привычку [{obj["action"]}]! "
         f"в [{obj["execution_time"]}] в [{obj["place"]}], "
         f"[{obj["periodicity"]}] раз в неделю! "
         f"Связанная привычка [{con_habit.action}]"
     )
-    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}")
+    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={telegram_chat_id}&text={message}")
 
 
 @shared_task
 def send_telegram_start_message(obj):
     """Отправляет сообщение, если не выбрано вознаграждение и связанная привычка."""
+    telegram_chat_id = User.objects.get(id=obj["owner"]).tg_id
     message = (
         f"Вы запланировали привычку [{obj["action"]}]! "
         f"в [{obj["execution_time"]}] в [{obj["place"]}], "
         f"[{obj["periodicity"]}] раз в неделю!"
     )
-    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}")
+    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={telegram_chat_id}&text={message}")
 
 
 @shared_task
 def send_telegram_reminder(message):
+    message = message.split(",")[0]
+    telegram_chat_id = int(message.split(",")[1])
     """Отправляет уведомление за 15 минут до времени выполнения привычки."""
-    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}")
+    requests.get(f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage?chat_id={telegram_chat_id}&text={message}")
